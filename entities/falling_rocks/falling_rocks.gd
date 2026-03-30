4f7d82e17097f469c37b228483477f33e2ef2d75
Tanoy <tanoy@macbook-air.mynetworksettings.com>
2026-03-30 17:23:25 -0400

    fallin rocks v1

---
+++ entities/falling_rocks/falling_rocks.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+enum State {
+	IDLE,
+	FALLING,
+	CRUMBLING
+}
+
+const height: int = -90
+const initial_speed: int = 2
+const acceleration: int = 1.6
+
+var speed: float = initial_speed
+var state: State = State.IDLE
+var root_position: Vector2 = Vector2.ZERO
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	root_position = self.global_position
+	reset()
+
+func _physics_process(delta: float) -> void:
+	if state == State.FALLING:
+		self.global_position.y += speed
+		speed += acceleration
+		if self.global_position.y >= root_position.y:
+			crumble()
+
+func fall() -> void:
+	state = State.FALLING
+	
+func crumble() -> void:
+	state = State.CRUMBLING
+	animated_sprite.play("Crumble")
+	await get_tree().create_timer(0.3).timeout
+	reset()
+
+func reset() -> void:
+	animated_sprite.play("Idle")
+	self.global_position.y += height
+	speed = initial_speed
+	await get_tree().create_timer(2.0).timeout
+	fall()
